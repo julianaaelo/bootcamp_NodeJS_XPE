f60726108b4bb79a460936305b0766ee89124536
julianaaelo <julianaaelo3@gmail.com>
2022-09-03 18:48:20 -0300

    Criação do método POST

--- modulo01_Apostila/myBankApi/routes/accounts.js
+++ modulo01_Apostila/myBankApi/routes/accounts.js
@@ -1,8 +1,27 @@
 // Importando express;
 import express from "express";
+import { promises as fs } from "fs";
+
+const { readFile, writeFile } = fs;
 
 // Criando objeto para roteador
 const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    // armazenando as informações do body;
+    let account = req.body;
+    // fazendo a leitura do arquivo json;
+    const data = JSON.parse(await readFile("accounts.json"));
+    // atualizando o id;
+    account = { id: data.nextId++, ...account };
+    // pegar o account do body e adicionar no parametro accounts do arquivo json;
+    data.accounts.push(account);
+    await writeFile("accounts.json", JSON.stringify(data));
+    console.log(account);
+    res.send(data);
+  } catch (err) {}
+});
 
 // Exportando o roteador;
 export default router;
